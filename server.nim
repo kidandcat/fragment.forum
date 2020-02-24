3cda0be4f290a1c8f79b17f7b9a8bee465251058
Jairo <kidandcat@gmail.com>
2020-02-24 15:08:40 +0100

    (server) now uses jester and jwt

--- server.nim
+++ server.nim
@@ -1,5 +1,24 @@
 import norm/sqlite
-import ws, asyncdispatch, asynchttpserver, json
+import 
+  asyncdispatch,
+  ws, 
+  ws/jester_extra, 
+  jester, 
+  json, 
+  chronicles, 
+  jwt, 
+  times, 
+  tables, 
+  nimcrypto
+  
+
+{.experimental: "codeReordering".}
+
+const LISTEN_PORT = Port(8081)
+info "Server listening on", port = repr(LISTEN_PORT)
+
+settings:
+  port = LISTEN_PORT
 
 db("msg.db", "", "", ""):
   type
@@ -16,30 +35,115 @@ db("msg.db", "", "", ""):
 withDb:
   createTables(force=true)
 
-var connections = newSeq[WebSocket]()
+var connections = newTable[string, WebSocket]()
+proc sen(ws: WebSocket, command: string, payload: JsonNode) {.async.} =
+  echo "<<<------", command, $payload
+  if ws.readyState == Open:
+    await ws.send($ %*{
+      "command": command,
+      "payload": payload
+    })
 
-proc cb(req: Request) {.async, gcsafe.} =
-  if req.url.path == "/ws":
+routes:
+  error Exception:
+    resp Http500, "Something bad happened: " & exception.msg
+  get "/ws":
+    var email = ""
     try:
-      var ws = await newWebSocket(req)
-      connections.add ws
+      var ws = await newWebSocket(request)
       withDb:
-        var bob = User(
-          name: "bob",
-          email: "bob@example.com",
-          password: "pass",
-          avatar: ""
-        )
-        bob.insert()
-        await ws.send($ %bob)
-      while ws.readyState == Open:
-        let packet = await ws.receiveStrPacket()
-        for other in connections:
-          if other.readyState == Open:
-            asyncCheck other.send(packet)
-    except:
-      echo ":: socket closed " & getCurrentExceptionMsg() & ":"
-  await req.respond(Http200, "Hello World")
+        while ws.readyState == Open:
+          let packet = await ws.receiveStrPacket()
+          var m = parseJson packet
+          var p = m{"payload"}
+          echo "------>>>", packet.substr(0, 100)
+          case m["command"].getStr:
 
-var server = newAsyncHttpServer()
-waitFor server.serve(Port(9001), cb)
+            # # # #
+            # JWT #
+            # # # #
+            of "jwt":
+              try:
+                let jwtToken = p["token"].getStr.toJWT()
+                if jwtToken.verify(secret):
+                  email = jwtToken.claims["email"].node.getStr
+                  discard User.getOne("email=?", [?email])
+                  connections[email] = ws
+                  await ws.sen("check", %*{
+                    "logged": true
+                  })
+                else:
+                  email = ""
+              except InvalidToken:
+                email = ""
+
+            # # # # #
+            # LOGIN #
+            # # # # #
+            of "login":
+              var lemail = p["email"].getStr
+              var password = p["password"].getStr
+              try:
+                var u = User.getOne(cond="email=?", params=[dbValue lemail])
+                if u.name == "":
+                  await ws.sen("loginerror", %*{
+                    "error": "El usuario no existe"
+                  })
+                  continue
+                if u.password == $keccak_256.digest(password):
+                  email = lemail
+                  connections[email] = ws
+                  await ws.sen("jwt", %*{
+                    "token": sign(lemail)
+                  })
+                else:
+                  await ws.sen("loginerror", %*{
+                    "error": "Contraseña incorrecta"
+                  })
+              except:
+                echo getCurrentExceptionMsg()
+                await ws.sen("loginerror", %*{
+                  "error": "El usuario no existe"
+                })
+
+    except:
+        echo email, " disconnected ", getCurrentExceptionMsg()
+        connections.del(email)
+
+
+# # # # # # #
+# SECURITY  #
+# # # # # # #
+var secret = "thenoobssecretisthemostpowerful"
+
+proc sign*(userId: string): string =
+  var exp = (getTime() + 1.days).toSeconds().int
+  debug "Generating JWT", userId = userId, expiration = exp
+  var token = toJWT(%*{
+    "header": {
+      "alg": "HS256",
+      "typ": "JWT"
+    },
+    "claims": {
+      "userId": userId,
+      "exp": exp
+    }
+  })
+  token.sign(secret)
+  result = $token
+
+proc verify*(token: string): bool =
+  debug "Verifying JWT", token = token
+  try:
+    let jwtToken = token.toJWT()
+    result = jwtToken.verify(secret)
+  except InvalidToken:
+    result = false
+
+proc decode*(token: string): string =
+  if verify token:
+    let jwt = token.toJWT()
+    result = jwt.claims["userId"].node.getStr
+    debug "Decoded JWT", userId = result
+  else:
+    raise newException(Exception, "Invalid JWT") 
